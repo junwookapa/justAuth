272a369fc918244c97583d743dad89e87034b217
junwoo <junwookapa@naver.com>
2015-09-08 10:48:47 +0900

    testJson

--- src/main/java/team/justtag/server/api/rest/HelloWorld.java
+++ src/main/java/team/justtag/server/api/rest/HelloWorld.java
@@ -4,12 +4,12 @@ import static spark.Spark.get;
 
 public class HelloWorld {
     public HelloWorld() {
-
     	// restAPI를 이용한 get test
     	// 사용 예 - http://todoapp-junwookapa.rhcloud.com/hello/준우/25
     	// 결과 - 이름: 준우 나이: 2
-    	get("/hello/:name/:age", (request, response) -> {
-    	    return "이름: " + request.params(":name") +"\n" +"나이: " + request.params(":age");
+    	get("/hello/:name/:age","application/json", (request, response) -> {
+    	   // return "이름: " + request.params(":name") +"\n" +"나이: " + request.params(":age");
+    	   return request.body().toString();
     	});
     }
 }
