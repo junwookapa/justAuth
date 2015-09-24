271c9dd8d8a6477296762a5133339535eea324a9
junwoo <junwookapa@naver.com>
2015-09-24 18:11:20 +0900

    ???

--- src/main/java/team/justtag/server/main/Main.java
+++ src/main/java/team/justtag/server/main/Main.java
@@ -3,15 +3,9 @@ package team.justtag.server.main;
 import static spark.SparkBase.setIpAddress;
 import static spark.SparkBase.setPort;
 import static spark.SparkBase.staticFileLocation;
-
-import java.util.List;
-
-import team.justtag.server.main.Status.UserStatus;
 import team.justtag.server.todo.controller.TodoResource;
 import team.justtag.server.todo.service.TodoService;
 import team.justtag.server.user.controller.UserController;
-import team.justtag.server.user.model.User;
-import team.justtag.server.user.service.UserService;
 import team.justtag.server.user.service.UserServiceImpl;
 
 import com.mongodb.DB;
@@ -40,7 +34,6 @@ public class Main {
             MongoClient mongoClient = new MongoClient("localhost");
             return mongoClient.getDB("justtagserver");//
         }
-        
         int port = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
         String dbname = System.getenv("OPENSHIFT_APP_NAME");
         String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
@@ -55,7 +48,7 @@ public class Main {
             throw new RuntimeException("Not able to authenticate with MongoDB");
         }
     }
-    
+    //
     private static void setController(DB db){
     	new TodoResource(new TodoService(db));
     	new UserController(new UserServiceImpl(db), db);
