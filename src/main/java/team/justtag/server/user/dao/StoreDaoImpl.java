8f1487fada3d3bde9931ef1c298bfdb41e8858bd
junwookapa <junwookapa@naver.com>
2015-09-24 23:51:56 +0900

    TEst

--- src/main/java/team/justtag/server/user/dao/StoreDaoImpl.java
+++ src/main/java/team/justtag/server/user/dao/StoreDaoImpl.java
@@ -1,5 +1,46 @@
 package team.justtag.server.user.dao;
 
-public class StoreDaoImpl {
+import java.util.List;
+
+import team.justtag.server.main.Status.DBStatus;
+import team.justtag.server.user.model.Store;
+
+public class StoreDaoImpl implements StoreDao{
+
+	@Override
+	public DBStatus createStore(Store store) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DBStatus updateStore(String _id, Store store) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DBStatus deleteUser(String _id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Store getStoreByObjectID(String _id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getObjIDBystoreName(String store_name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Store> getAllStores() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
