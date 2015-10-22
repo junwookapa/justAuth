c6330a8024605a767f0c0fbd5e1f26df5cf45508
junwoo <junwookapa@naver.com>
2015-10-22 17:02:46 +0900

    Preparing test

--- src/main/java/team/justauth/server/main/Main.java
+++ src/main/java/team/justauth/server/main/Main.java
@@ -20,7 +20,7 @@ import com.mongodb.WriteConcern;
  * Created by shekhargulati on 09/06/14.
  */
 public class Main {
-    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "192.168.10.40";
+    private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
     private static final int PORT = System.getenv("OPENSHIFT_DIY_PORT") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_PORT")) : 8080;
 
     public static void main(String[] args) throws Exception {
